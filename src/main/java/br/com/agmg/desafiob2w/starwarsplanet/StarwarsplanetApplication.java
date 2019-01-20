d4b608d03121fb8603a8e78761e495354741845e
AlexandreGuerra <agmguerra@gmail.com>
2019-01-20 20:17:40 -0200

    versão inicial

---
+++ src/main/java/br/com/agmg/desafiob2w/starwarsplanet/StarwarsplanetApplication.java
@@ -0,0 +1,14 @@
+package br.com.agmg.desafiob2w.starwarsplanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarwarsplanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarwarsplanetApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/br/com/agmg/desafiob2w/starwarsplanet/StarwarsplanetApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.agmg.desafiob2w.starwarsplanet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StarwarsplanetApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
