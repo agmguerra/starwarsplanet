da81ba839d92bdc9d186f44f5f2329d51ed00143
AlexandreGuerra <agmguerra@gmail.com>
2019-01-30 00:40:37 -0200

    ajustes e testes

---
+++ src/test/java/br/com/agmg/desafiob2w/starwarsplanet/service/StarwarsPlanetAllTests.java
@@ -0,0 +1,11 @@
+package br.com.agmg.desafiob2w.starwarsplanet.service;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ PlanetServiceTests.class, StarwarsApiServiceTests.class })
+public class StarwarsPlanetAllTests {
+	
+}
